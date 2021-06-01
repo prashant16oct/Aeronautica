c38f04983e2d5af2d8b19888c512a34cbbb08492
Prashant Sharma <51667502+prashant16oct@users.noreply.github.com>
2021-06-01 16:19:54 +0530

    Add files via upload

--- animation.js
+++ animation.js
@@ -1,1 +1,6 @@
-gsap.to("#logo-anime", {rotationY:360, duration:3, repeat:-1, repeatDelay:5})
+gsap.to("#logo-anime", {rotationY:360, delay:2, duration:3, repeat:-1, repeatDelay:5});
+
+gsap.to(".heading3", { rotationX:360, duration:3, ease:"bounce", delay:2});
+
+gsap.to(".drone", { x:"68vw", repeat:-1, yoyo:"true",ease:"power1.inOut", duration:4})
+
