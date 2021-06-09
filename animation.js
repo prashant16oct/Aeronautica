a87765a615c618462a714a96edd7ce887301f345
Prashant Sharma <51667502+prashant16oct@users.noreply.github.com>
2021-06-09 16:24:51 +0530

    Update animation.js

--- animation.js
+++ animation.js
@@ -2,7 +2,7 @@ gsap.to("#logo-anime", {rotationY:360, delay:2, duration:3, repeat:-1, repeatDel
 
 gsap.to(".heading3", { rotationX:360, duration:3, ease:"bounce", delay:2});
 
-gsap.fromTo("#drone",{x:0, y:0, scale:1 }, { x:"63vw", y:"18px", scale:0.2, repeat:-1,
+gsap.fromTo("#drone",{x:0, y:0, scale:1 }, { x:"60vw", y:"18px", scale:0.2, repeat:-1,
  yoyo:"true",ease:"power1.inOut", duration:12})
 
 // gsap.to(".div-box", {rotation:360, duration:6, repeat:-1, yoyo:"true", ease:"power2.inOut", scale:1.8})
